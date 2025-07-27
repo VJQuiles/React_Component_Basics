6a4cf57feb3e51de7ff62879de5b56a1a444a32b
Vince Quiles <vjquiles@msn.com>
2025-07-27 13:12:31 -0400

    I realized i put my user profile files in the product display and vice vera with product display files lol. Got the UserProfileCard component working. Almost one to one, had to better understand a couple tools we used like React.FC and passing props to components. The example helped to maintain perspective though.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
 import './App.css'
-import { AlertBox } from './components/AlertBox/AlertBox'
+import { UserProfileCard } from './components/ProductDisplay/UserProfileCard'
+// import { AlertBox } from './components/AlertBox/AlertBox'
 
 function App() {
 
+  const user = {
+    id: '1',
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    role: 'Software Engineer',
+    avatarUrl: 'https://example.com/avatar.jpg'
+  }
+
   return (
     <div className='container m-4'>
-      <AlertBox
+      {/* <AlertBox
         type="success"
         message='This was a success'
         onClose={() => alert('Time to close this jawn down.')}>
         <p className='text-sm'>
           You can now continue using the application that doesn't really do anything 🫣.
         </p>
-      </AlertBox>
+      </AlertBox> */}
+
+      <UserProfileCard
+        user={user}
+        showEmail={true}
+        showRole={true}
+        onEdit={(userId) => alert(`Once you change your name user ${userId}, we will unneccessarily charge you to change it again.`)}>
+        <div className='text-sm text-muted'>
+          Last Seen before Touching Grass: 2 hours ago
+        </div>
+      </UserProfileCard>
     </div>
   )
 }
---
+++ src/components/ProductDisplay/UserProfileCard.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import type { UserProfileCardProps } from "../../types"
+
+export const UserProfileCard: React.FC<UserProfileCardProps> = ({
+    user,
+    showEmail,
+    showRole,
+    onEdit,
+    children,
+}) => {
+
+    return (
+        <div className="card">
+            <div className="body">
+                <h5 className="card-title">{user.name}</h5>
+                {showEmail && <p className="card-text">{user.email}</p>}
+                {showRole && <p className="card-text">{user.role}</p>}
+                {onEdit && (
+                    <button
+                        className="btn btn-primary"
+                        onClick={() => onEdit(user.id)}>
+                        Edit
+                    </button>
+                )}
+                <div>
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
